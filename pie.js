7f1bfa51fac1bad49f5c985380f817c196fd51a1
dperolio <dustin.perolio@outlook.com>
2021-10-09 10:08:16 -0400

    Update pie.js

--- pie.js
+++ pie.js
@@ -1,2 +1,5 @@
 console.log('i like pie!');
 window.pie = 'pie';
+const style = document.createElement('style');
+style.appendChild(document.createTextNode(`* { color: teal; }`));
+document.body.appendChild(style);
