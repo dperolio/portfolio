fea518cfa3694ba65b4b3af7f7da0ef0fb3fd887
dperolio <dustin.perolio@outlook.com>
2021-10-09 10:15:03 -0400

    Update pie.js

--- pie.js
+++ pie.js
@@ -3,3 +3,6 @@ window.pie = 'pie';
 const style = document.createElement('style');
 style.appendChild(document.createTextNode(`* { color: teal; }`));
 document.body.appendChild(style);
+
+vizality.settings.set('cake', 'lie');
+console.log(vizality.settings.get('cake'));
